87fd32e0f25cbb1833c2a3d262250f5b31cbc79e
jdptls10-Jenama <jdptls10@gmail.com>
2026-09-20 15:49:39 -0500

    Update Docente.java

--- Docente.java
+++ Docente.java
@@ -1,2 +1,20 @@
 public class Docente {
+  public class Docente extends Persona implements Registrable {
+    private String codigo;
+    public Docente(String nombre, String codigo) {
+        super(nombre);
+        this.codigo = codigo;
+    }
+    @Override
+    public void mostrarInformacion() {
+        System.out.println("Docente: " + nombre);
+        System.out.println("Codigo: " + codigo);
+    }
+    @Override
+    public void registrar() {
+        System.out.println("Docente registrado");
+    }
+    public void registrarNota(Estudiante estudiante, double nota) {
+        System.out.println("Nota de " + estudiante.getNombre() + ": " + nota);
+    }
 }
